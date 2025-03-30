555b58e6e7888af6ac3b62b48989aab7a47335c7
Sahilb315 <bansalsahil315@gmail.com>
2025-03-31 02:23:10 +0530

    switched to nix-unstable & small config changes

--- .config/hypr/UserScripts/sleep.sh
+++ .config/hypr/UserScripts/sleep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-swayidle -w \
-  timeout 300 'swaylock -f -c 000000' \  # Lock screen after 5 minutes
-  timeout 600 'systemctl suspend' \      # Suspend after 10 minutes
-  before-sleep 'swaylock -f -c 000000' &  # Lock before sleep
+ hypridle -w \
+   timeout 10 'hyprlock -f' \  # Lock screen after 5 minutes
+   timeout 600 'systemctl suspend' \  # Suspend after 10 minutes
+   before-sleep 'hyprlock -f' &
